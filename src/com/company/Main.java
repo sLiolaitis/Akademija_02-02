ec3e22849246a9472d649b9dc5afcc7f09c9bb02
sLiolaitis <74714575+sLiolaitis@users.noreply.github.com>
2021-02-04 11:58:57 +0200

    Upadate on main method
    Uncommented of method calls in main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,22 +13,22 @@ public class Main {
     public static void main(String[] args) {
 
         Basics basics = new Basics();
-        // basics.hello();
+         basics.hello();
          basics.task2();
-        // basics.task3();
+        basics.task3();
 
         Arithmetics arithmetics = new Arithmetics();
-        //arithmetics.task4();
-       // arithmetics.task5();
+        arithmetics.task4();
+        arithmetics.task5();
 
         Operators operators = new Operators();
-        // operators.task6();
-        //operators.task7();
+         operators.task6();
+        operators.task7();
 
         Loops loops = new Loops();
-        //loops.task8();
-        //loops.task9();
-        //loops.task10();
+        loops.task8();
+        loops.task9();
+        loops.task10();
 
 
 
